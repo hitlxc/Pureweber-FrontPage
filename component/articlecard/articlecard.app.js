bea6286440403f7cfe8227452f5ab306f3a2e466
tmn07 <519043202@qq.com>
2016-12-04 14:02:15 +0800

    ajax

--- component/articlecard/articlecard.app.js
+++ component/articlecard/articlecard.app.js
@@ -7,16 +7,28 @@ import ArticleCard from './articlecard.entry'
 import $ from 'jquery';
 
 $.get("http://localhost:3000/blog/gets",{
-	num:2,
+	num:5,
 	page:1
 },function(result){
 		console.log(result);
+		for (var i = result.length - 1; i >= 0; i--) {
+			var tit = result[i].title;
+			var author = result[i].uid.toString();
+			console.log(author);
+
+			let app = document.createElement('div');
+		
+			ReactDOM.render(<ArticleCard author={result[i].uid} tag={result[i].cid} avatar='http://p3.wmpic.me/article/2015/03/16/1426483394_eJakzHWr.jpeg'  title={result[i].title} abstract={result[i].content} pic='https://www.baris-sagdic.com/file/2016/06/javascript-1.png' />, app);
+			document.body.appendChild(app);
+
+		}
+
 })
 
-let app = document.createElement('div');
-ReactDOM.render(<ArticleCard author='hitlxc' tag='js' avatar='http://p3.wmpic.me/article/2015/03/16/1426483394_eJakzHWr.jpeg' title='文章标题' abstract='文章摘要。。。。。。。。' pic='https://www.baris-sagdic.com/file/2016/06/javascript-1.png' />, app);
-document.body.appendChild(app);
-
+// hitlxc
+// let app = document.createElement('div');
+// ReactDOM.render(<ArticleCard author='hitlxc' tag='js' avatar='http://p3.wmpic.me/article/2015/03/16/1426483394_eJakzHWr.jpeg' title='文章标题' abstract='文章摘。。' pic='https://www.baris-sagdic.com/file/2016/06/javascript-1.png' />, app);
+// document.body.appendChild(app);
 
 
 
