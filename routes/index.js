722dee5a3ca3a8014e8fcc887c5b754319b1fafb
tmn07 <519043202@qq.com>
2016-12-20 21:56:04 +0800

    判断登陆

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-	res.render('index', { title: 'Pureweber' , article:['文章1','文章2','文章3'], logged:false});
+
+	if (typeof req.session.uid === 'undefined') {
+		log_status = false;
+	}
+	else{
+		log_status = true;
+	}
+
+	res.render('index', { title: 'Pureweber' , article:['文章1','文章2','文章3'], logged:log_status});
 });
 
 module.exports = router;
